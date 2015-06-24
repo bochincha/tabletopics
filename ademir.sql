a0bfbc6abf5347de38336ed8ca67d25544d261f2
Matthew Russell <mrussell@neptec.com>
2015-06-24 15:25:39 -0400

    Reverting commit 12916b92be07ba7846006df8fba05b1c7dfd9d2d : "Removed some features from the schema in order to make it compatible with sqlite3"

--- ademir.sql
+++ ademir.sql
@@ -1,21 +1,23 @@
+SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS opinion;
 DROP TABLE IF EXISTS dialog;
+SET foreign_key_checks = 1;
 
 CREATE TABLE dialog (
-   dialog_id int NOT NULL,
-   table_id int,
+   dialog_id int NOT NULL AUTO_INCREMENT,
+	table_id int,
    speaker_id varchar(10),
    utterance text,
-   comment text,
+   comment text COMMENT 'Ademir\'s comment on utterance',
    PRIMARY KEY(`dialog_id`)
 );
 
 CREATE TABLE opinion (
-   opinion_id int NOT NULL,
+   opinion_id int NOT NULL AUTO_INCREMENT,
    dialog_id int NOT NULL,
-   opt char(1) DEFAULT '1',
+   opt char(1) DEFAULT '1' COMMENT 'The option from the spreadsheet, normally pro/con',
    tag varchar(100),
-   code int DEFAULT 0,
+   code int DEFAULT 0 COMMENT 'ENUM, opinon from -2 to 2, 4=dunno',
    PRIMARY KEY(`opinion_id`),
    CONSTRAINT `opinion_FK_1`
       FOREIGN KEY(`dialog_id`)
