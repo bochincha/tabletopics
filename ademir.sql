26a26083686c630e3ef4ab5e07b808fc3be9c3f8
bochincha <bochincha@users.noreply.github.com>
2015-06-24 10:14:50 -0400

    Update ademir.sql
    Fixed syntax for HSQLDB

--- ademir.sql
+++ ademir.sql
@@ -1,25 +1,25 @@
 DROP TABLE IF EXISTS opinion;
 DROP TABLE IF EXISTS dialog;
 
-CREATE TABLE dialog (
-   dialog_id int NOT NULL,
-   table_id int,
-   speaker_id varchar(10),
-   utterance text,
-   comment text,
-   PRIMARY KEY(`dialog_id`)
+CREATE TABLE "dialog" (
+   "dialog_id" integer NOT NULL,
+   "table_id" integer,
+   "speaker_id" varchar(10),
+   "utterance" varchar(1000),
+   "comment" text,
+   PRIMARY KEY("dialog_id")
 );
 
-CREATE TABLE opinion (
-   opinion_id int NOT NULL,
-   dialog_id int NOT NULL,
-   opt char(1) DEFAULT '1',
-   tag varchar(100),
-   code int DEFAULT 0,
-   PRIMARY KEY(`opinion_id`),
-   CONSTRAINT `opinion_FK_1`
-      FOREIGN KEY(`dialog_id`)
-      REFERENCES `dialog` (`dialog_id`)
+CREATE TABLE "opinion" (
+   "opinion_id" integer NOT NULL,
+   "dialog_id" integer NOT NULL,
+   "opt" char(1) DEFAULT 1,
+   "tag" varchar(100),
+   "code" integer DEFAULT 0,
+   PRIMARY KEY("opinion_id"),
+   CONSTRAINT "opinion_FK_1"
+      FOREIGN KEY("dialog_id")
+      REFERENCES "dialog" ("dialog_id")
       ON DELETE CASCADE
 );
 
