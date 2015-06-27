640d3217cca0a5285753e85888ea98e964aa68e5
bochincha <bochincha@users.noreply.github.com>
2015-06-26 22:03:54 -0400

    Importance of Topic column

--- importSheet.py
+++ importSheet.py
@@ -20,7 +20,7 @@ prune = False;
 table_id = 1
 
 # The file we're reading in
-csvfile = "sample.csv"
+csvfile = "Samplesheet.csv"
 
 try:
 	# Short option syntax: "hv:"
@@ -46,13 +46,13 @@ for option, argument in opts:
 		Clears all data in database before starting
 
 Please note, this file was written quickly, designed without flexibility in mind, implemented in the most basic way practical, and was only tested on one sheet."""
-
+		
 	elif option == "-t":
 		table_id = int(argument)
-
+		
 	elif option == "-f":
 		csvfile = argument
-
+		
 	elif option in ("-v", "--prune"):
 		prune = True
 
@@ -75,9 +75,9 @@ def formatUtterance(str):
 # http://stackoverflow.com/questions/372885/how-do-i-connect-to-a-mysql-database-in-python
 db = MySQLdb.connect(  \
 	host   = "localhost", # your host, usually localhost \
-	user   = "ademir", \
-	passwd = "ademir", \
-	db     = "ademir")
+	user   = "root", \
+	passwd = "somethingsimple", \
+	db     = "ECASTv1")
 
 # This cursor will execute all the queries you need
 cur = db.cursor()
@@ -107,8 +107,8 @@ with open(csvfile, 'rb') as csvfile:
 
 	for row,line in enumerate(dialogs):
 		# Skip the first two lines, no data there
-		if row<=2:
-			continue
+		if row<=1:
+			continue 
 
 		# If the first cell has content, consider this the start of a new line
 		# of dialog
@@ -117,17 +117,16 @@ with open(csvfile, 'rb') as csvfile:
 			#print "Processing dialog for %s"%(line[0])
 			speaker   = db.escape_string(formatSpeaker(line[0]))
 			utterance = db.escape_string(formatUtterance(line[1]))
-			comment   = db.escape_string(line[7])
+			comments   = db.escape_string(line[7])
 
 			try:
 				# Save this dialog
-				query = "INSERT INTO dialog (table_id, speaker_id, utterance, comment) VALUES(%d, '%s', '%s', '%s')"%(table_id, speaker, utterance, comment)
+				query = "INSERT INTO dialog (table_id, speaker_id, utterance, comments) VALUES(%d, '%s', '%s', '%s')"%(table_id, speaker, utterance, comments)
 				cur.execute(query)
 				db.commit()
-			except Exception as e:
+			except:
 				# Rollback in case there is any error
-				print "Something went wrong inserting into dialog"
-				print e
+				print "Something went wrong inserting into dialog (line129)"
 				db.rollback()
 				exit(0);
 
@@ -148,20 +147,40 @@ with open(csvfile, 'rb') as csvfile:
 		if len(line[2]) and len(line[3]):
 			tag = db.escape_string(formatTag(line[2]))
 			code = int(line[3])
-			# Save this dialog
-			query = "INSERT INTO opinion (dialog_id, opt, tag, code) VALUES(%d, 1, '%s', %d)"%(dialog_id, tag, code)
-			#print query
-			cur.execute(query)
-			db.commit()
+			# Check to see if the importance column has something in it
+			if len(line[6]):
+				importance = int(line[6])
+				# Save this dialog
+				query1 = "INSERT INTO opinion (dialog_id, opt, tag, code, importance) VALUES(%d, 'A', '%s', %d, %d)"%(dialog_id, tag, code, importance)
+				#print query
+				cur.execute(query1)
+				db.commit()
+			else: 
+				#Just save the dialog per original code
+				query = "INSERT INTO opinion (dialog_id, opt, tag, code) VALUES(%d, 'A', '%s', %d)"%(dialog_id, tag, code)
+				#print query
+				cur.execute(query)
+				db.commit()
 
 		# Check to see if we have an opinion
 		if len(line[4]) and line[5]:
 			tag = db.escape_string(formatTag(line[4]))
 			code = int(line[5])
-			# Save this dialog
-			query = "INSERT INTO opinion (dialog_id, opt, tag, code) VALUES(%d, 2, '%s', %d)"%(dialog_id, tag, code)
-			cur.execute(query)
-			db.commit()
+			# Check to see if the importance column has something in it
+			if len(line[6]):
+				importance = int(line[6])
+				# Save this dialog
+				query2 = "INSERT INTO opinion (dialog_id, opt, tag, code, importance) VALUES(%d, 'B', '%s', %d, %d)"%(dialog_id, tag, code, importance)
+				# Print query
+				cur.execute(query2)
+				db.commit()
+			else:
+				#Just save the dialog per original code
+				query = "INSERT INTO opinion (dialog_id, opt, tag, code) VALUES(%d, 'B', '%s', %d)"%(dialog_id, tag, code)
+				# Print query
+				cur.execute(query)
+				db.commit()
+
 
 		#print "%d:"%(row) + ', '.join(line)
 
